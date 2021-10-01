dbc510bfd4f4ef278876cf1d80d17e13c9e01e25
yesman9692 <yws090296@naver.com>
2021-10-01 17:48:23 +0900

    something change

--- src/main/java/com/igloo/wonseok/hellospring/SpringConfig.java
+++ src/main/java/com/igloo/wonseok/hellospring/SpringConfig.java
@@ -1,15 +1,25 @@
 package com.igloo.wonseok.hellospring;
 
+import com.igloo.wonseok.hellospring.repository.JdbcTemplateMemberRepository;
 import com.igloo.wonseok.hellospring.repository.MemberRepository;
 import com.igloo.wonseok.hellospring.repository.MemoryMemerRepository;
 import com.igloo.wonseok.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
 
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository());
@@ -17,6 +27,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemerRepository ();
+        //return new MemoryMemerRepository ();
+        return new JdbcTemplateMemberRepository(dataSource);
     }
 }
